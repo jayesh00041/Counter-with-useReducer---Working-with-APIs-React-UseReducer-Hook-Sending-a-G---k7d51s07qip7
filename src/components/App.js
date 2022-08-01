840ca7cf12d6715b3ddfc1b666496d0f4316553f
jayesh00041 <jayesh00041@gmail.com>
2022-08-01 16:17:43 +0530

    hgfh

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ const [state,dispatch] = useReducer(counterReducer,0)
   return (
     <div id="main">
       <span id="counter">{state}</span>
-      <button id="increment-btn" onClick={dispatch({type : 'increment'})}></button>
+      <button id="increment-btn" onClick={()=>dispatch({type : 'increment'})}>Increment</button>
+      <button id="decrement-btn" onClick={()=>dispatch({type : 'decrement'})}>Decrement</button>
     </div>
   )
 }
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,5 @@
 const counterReducer = (state,action) =>{
-    switch(action){
+    switch(action.type){
         case 'increment': return state+1;
         case 'decrement': return state-1;
     }
